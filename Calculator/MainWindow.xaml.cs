b8425c19c1a1a423c2ae08b9c8e0cf010056d068
wWrichey <warunyoo.wcd@gmail.com>
2024-11-03 11:35:49 +0700

    Add project files.

---
+++ Calculator/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Calculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void sum1_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Calculated!!!");
+
+            allSum1.Text = (float.Parse(txtBox3.Text) / (float.Parse(txtBox1.Text) - float.Parse(txtBox2.Text))).ToString();
+        }
+    }
+}
